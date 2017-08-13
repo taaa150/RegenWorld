5423e5519f741651ebcbcb4c2636016e02680d35
Mon_chi <poyo0315@gmail.com>
2017-08-13 19:38:31 +0900

    Initial Commit

---
+++ src/main/java/io/monchi/regenworld/RegenWorld.java
@@ -0,0 +1,16 @@
+package io.monchi.regenworld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RegenWorld extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
